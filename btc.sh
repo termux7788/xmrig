9aee6bd6eb5a27aa0b8af5a733f2a482f267c317
termux7788 <108569917+termux7788@users.noreply.github.com>
2024-10-05 20:52:17 +0600

    Add files via upload

---
+++ btc.sh
@@ -0,0 +1,5 @@
+cd xmrig
+
+cd build
+
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u BTC:15nsn3NH1635n9bvBZNB2tLiSYngxLxBvm.unmineable_worker_ptwphdh -p x 
---
+++ usd.sh
@@ -0,0 +1,5 @@
+cd xmrig
+
+cd build
+
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u USDT:0xd4881fafea0358021a7c86b4a316cb0dbee2690b.unmineable_worker_ohswel -p x
